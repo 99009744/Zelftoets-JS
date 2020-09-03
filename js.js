ca4396f94d3eafc187d94c5015ed1e44542c1dbb
99009744 <55435447+99009744@users.noreply.github.com>
2020-09-03 17:52:23 +0200

    Miss counter aangemaakt
    Krijg hem buiten console niet werkend.
    + total van 20 shots toegevoegd

--- js.js
+++ js.js
@@ -1,6 +1,7 @@
 var hits = 0;
-var miss = 0;
+var shotmiss = 0;
 var shots = 20;
+var total;
 
 var shots = document.createElement("div");
 shots.id = "shots";
@@ -11,10 +12,10 @@ hit.id = "hit";
 hit.innerHTML = "HITS: " + hits;
 document.getElementById("shots").appendChild(hit);
 
-var misss = document.createElement("h1");
-misss.id = "miss";
-misss.innerHTML = "MISS: " + miss;
-document.getElementById("shots").appendChild(misss);
+var miss = document.createElement("h1");
+miss.id = "miss";
+miss.innerHTML = "MISS: " + shotmiss;
+document.getElementById("shots").appendChild(miss);
 
 var duck = document.createElement("img");
 duck.src = "duck.gif";
@@ -25,4 +26,16 @@ document.getElementById("fullpage").appendChild(duck);
 function duckhit(){
     hits++;
     hit.innerHTML = "HITS: " + hits;
+    total = hits + shotmiss;
+    console.log(total);
+    if(total == 20){
+        alert("Je kogels zijn op, je hebt " + hits + "x de eend geraakt en " + shotmiss + "x gemist");
+        hits = 0;
+        miss = 0;
+    }
+}
+function missclick(){
+    shotmiss++;
+    miss.innerHTML = "MISS: " + shotmiss;
+    console.log(shotmiss);
 }
